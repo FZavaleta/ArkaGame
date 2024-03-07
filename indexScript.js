4b04f52c6372a791140415cd5ac637173f15d8ca
Fabrizio Zavaleta <fabriziozch@gmail.com>
2024-03-07 14:33:38 -0500

    Se dibujo la pelo del juego.

--- indexScript.js
+++ indexScript.js
@@ -1,5 +1,42 @@
+/* Configuracion general */
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+
+let counter = 0;
+/*Variables de las pelotas*/
+const ballRadius = 3;
+let x = canvas.width / 2;
+let y = canvas.height / 2;
+let dx = 2;
+let dy = -2;
+
+function drawBall(){
+    ctx.beginPath();
+    ctx.arc(x,y,ballRadius,0, Math.PI*2);
+    ctx.fillStyle = '#fff';
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPaddle(){
+
+}
+
+function drawBricks(){
+
+}
+
+function collisionDetection(){
+
+}
+
+function ballMovement(){
+
+}
+
+function paddleMovement(){
+
+}
 
 canvas.width = 448;
 canvas.height = 512;
