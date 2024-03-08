11e2394ce447130556916d9db89aaa43f91448d4
Fabrizio Zavaleta <fabriziozch@gmail.com>
2024-03-08 16:19:46 -0500

    Se creo el paddle y se incializo un evento de escucha para pode registrar las acciones del usuario.

--- indexScript.js
+++ indexScript.js
@@ -2,33 +2,46 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+/*Variables generales del juego */
 let counter = 0;
-/*Variables de las pelotas*/
+
+/*VARIABLES DE LA PELOTA*/
 const ballRadius = 3;
-let x = canvas.width / 2;
-let y = canvas.height / 2;
+//Posicion inicial de la pelota
+let x = canvas.width / 1.2;
+let y = canvas.height + 300;
+//Velocidad de pelota
 let dx = 2;
 let dy = -2;
 
+/*VARIABLE DE LA PALETA*/
+const paddleHeight = 10;
+const paddleWidth = 50;
+/*Posiciones iniciales de la paleta*/
+let paddleX = (canvas.width - paddleWidth) / 2;
+let paddleY = canvas.height - paddleHeight -10
+
+
 function drawBall(){
     ctx.beginPath();
-    ctx.arc(x,y,ballRadius,0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = '#fff';
     ctx.fill();
     ctx.closePath();
 }
 
 function drawPaddle(){
+    ctx.fillReact(
+        paddleX,
+        paddleY,
+        paddleWidth,
+        paddleHeight
+    )
+ }
 
-}
+function drawBricks(){ }
 
-function drawBricks(){
-
-}
-
-function collisionDetection(){
-
-}
+function collisionDetection(){ }
 
 function ballMovement(){
     //Rebote cuando llegue a los limites laterales
@@ -46,7 +59,6 @@ function ballMovement(){
     if(y + dy > canvas.height - ballRadius){
         console.log("Game Over");
         document.location.reload()
-
     }
 
     x += dx;
@@ -60,6 +72,11 @@ function paddleMovement(){
 
 function cleanCanvas(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+function initEvents(){
+    document.addEventListener('keydown', keyDownHandler);
+    document.addEventListener('keydown', keyUpHandler);
 }
 
 canvas.width = 448;
